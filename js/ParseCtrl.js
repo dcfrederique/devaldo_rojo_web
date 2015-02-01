7270c6fc0300c6c35207903c824be483c7ba5d46
dcfrederique <dcfrederique@gmail.com>
2015-02-01 18:36:53 +0100

    +parse

--- js/ParseCtrl.js
+++ js/ParseCtrl.js
@@ -2,9 +2,18 @@
  * Created by Fre on 30/01/15.
  */
 var testApp = angular.module('devaldo_rojo_web_app', []);
+testApp.filter("toArray", function(){
+    return function(obj) {
+        var result = [];
+        angular.forEach(obj, function(val, key) {
+            result.push(val);
+        });
+        return result;};})
 
 testApp.controller('ParseCtrl', function ($scope,$http) {
     $scope.players = [];
+    $scope.kalender = [];
+    $scope.ranking = [];
     $scope.getPlayers = function() {
 
         $http.get('https://api.parse.com/1/classes/Speler', {headers: {
@@ -17,5 +26,32 @@ testApp.controller('ParseCtrl', function ($scope,$http) {
             .error(function(data, status) {
                 alert(status);
             });
-    }
+    };
+
+    $scope.getKalender = function(){
+        $http.get('https://api.parse.com/1/classes/MatchCalendar', {headers: {
+            'X-Parse-Application-Id':'KvXR4nBYYeZ4BSJCC5s7AInIHL2qjDZtMi9wFa1A',
+            'X-Parse-REST-API-Key':'VTcRHUDuZ2dq3f4IpUIww29KNbdUeigbNgOxpz7S',
+            'Content-Type' : 'application/json'}})
+            .success(function(data, status) {
+
+                $scope.kalender = data;
+            })
+            .error(function(data, status) {
+                alert(status);
+            });
+    };
+
+    $scope.getRanking = function(){
+        $http.get('https://api.parse.com/1/classes/Ranking', {headers: {
+            'X-Parse-Application-Id':'KvXR4nBYYeZ4BSJCC5s7AInIHL2qjDZtMi9wFa1A',
+            'X-Parse-REST-API-Key':'VTcRHUDuZ2dq3f4IpUIww29KNbdUeigbNgOxpz7S',
+            'Content-Type' : 'application/json'}})
+            .success(function(data, status) {
+                $scope.ranking = data;
+            })
+            .error(function(data, status) {
+                alert(status);
+            });
+    };
 });
